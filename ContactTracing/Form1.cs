c5fd589d8d219185fb11a9b2c1d2ae91eda26ef2
JohanJVillanueva <johanjvillanueva@iskolarngbayan.pup.edu.ph>
2022-06-08 22:55:43 +0800

    Added replace function to the textboxes who does not need numbers as input

--- ContactTracing/Form1.cs
+++ ContactTracing/Form1.cs
@@ -23,8 +23,13 @@ namespace ContactTracing
             byte correct = 0;
             //Name
             string Fname = txtFName.Text;
+            Fname = Fname.Replace("1", "").Replace("2", "").Replace("3", "").Replace("4", "").Replace("5", "").Replace("6", "").Replace("7", "").Replace("8", "").Replace("9", "").Replace("0", "");
+
             string MI = txtMI.Text;
+            MI = MI.Replace("1", "").Replace("2", "").Replace("3", "").Replace("4", "").Replace("5", "").Replace("6", "").Replace("7", "").Replace("8", "").Replace("9", "").Replace("0", "");
+
             string Lname = txtLName.Text;
+            Lname = Lname.Replace("1", "").Replace("2", "").Replace("3", "").Replace("4", "").Replace("5", "").Replace("6", "").Replace("7", "").Replace("8", "").Replace("9", "").Replace("0", "");
 
             //Address
             string StreetNum = txtStNo.Text;
@@ -43,21 +48,40 @@ namespace ContactTracing
             string Minute = txtMin.Text;
             string Meridian = txtAMPM.Text;
             
-            while(correct < 9)
+            while(correct != 12)
             {
+                //Input check for First Name
+                if (Fname.Length == 0)
+                {
+                    MessageBox.Show("Missing Info on First Name! Submit again once fixed.");
+                    break;
+                    //Makes the first letter upper case
+                }
+                else if (Fname.Length > 1) {
+                    Fname = char.ToUpper(Fname[0]) + Fname.Substring(1);
+                    correct++;
+                }
+
+                //Input check for Middle Initial
+                if (MI.Length == 0) { 
+                    MessageBox.Show("Missing Info on Middle Initial! Submit again once fixed.");
+                    break;
+                }
+                else if (MI.Length > 1)
+                {
+                    correct++;
+                }
 
             }
-            if (Fname.Length == 0)
-                MessageBox.Show("Missing Info on First Name!");
-            else if (Fname.Length > 1)
-                char.ToUpper(Fname[0]);
 
-            MessageBox.Show(Fname);
+            //MessageBox.Show(Fname);
 
 
-            StreamWriter file = new StreamWriter(@"E:\Programming\ContactTracing\ContactTracing\ContactTrace.txt");
-            file.WriteLine("Testing1");
-            file.Close();
+            if (correct == 12) { 
+                StreamWriter file = new StreamWriter(@"E:\Programming\ContactTracing\ContactTracing\ContactTrace.txt");
+                file.WriteLine("Testing1");
+                file.Close();
+            }
         }
 
     }
