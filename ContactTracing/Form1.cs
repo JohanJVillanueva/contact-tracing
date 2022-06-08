382786a21cd434be10ee6dfdf49e0ecf18db96b5
JohanJVillanueva <johanjvillanueva@iskolarngbayan.pup.edu.ph>
2022-06-08 23:08:28 +0800

    Added all the replace properties to each name contact and address variable, will be fixing date and time variable now

--- ContactTracing/Form1.cs
+++ ContactTracing/Form1.cs
@@ -23,16 +23,41 @@ namespace ContactTracing
             byte correct = 0;
             //Name
             string Fname = txtFName.Text;
+            //Numbers
             Fname = Fname.Replace("1", "").Replace("2", "").Replace("3", "").Replace("4", "").Replace("5", "").Replace("6", "").Replace("7", "").Replace("8", "").Replace("9", "").Replace("0", "");
+            //Symbols
+            Fname = Fname.Replace("%", "").Replace("^", "").Replace("&", "").Replace("*", "").Replace("(", "").Replace(")", "").Replace("_", "").Replace("=", "").Replace("+", "");
+            Fname = Fname.Replace("\\", "").Replace("|", "").Replace("<", "").Replace(">", "").Replace(",", "").Replace(";", "").Replace(":", "").Replace("[", "").Replace("]", "");
+            Fname = Fname.Replace("{", "").Replace("}", "").Replace("'", "").Replace("!", "").Replace("@", "").Replace("#", "").Replace("$", "");
+
 
             string MI = txtMI.Text;
+            //Numbers
             MI = MI.Replace("1", "").Replace("2", "").Replace("3", "").Replace("4", "").Replace("5", "").Replace("6", "").Replace("7", "").Replace("8", "").Replace("9", "").Replace("0", "");
+            //Symbols
+            MI = MI.Replace("%", "").Replace("^", "").Replace("&", "").Replace("*", "").Replace("(", "").Replace(")", "").Replace("_", "").Replace("=", "").Replace("+", "");
+            MI = MI.Replace("\\", "").Replace("|", "").Replace("<", "").Replace(">", "").Replace(",", "").Replace(";", "").Replace(":", "").Replace("[", "").Replace("]", "");
+            MI = MI.Replace("{", "").Replace("}", "").Replace("'", "").Replace("!", "").Replace("@", "").Replace("#", "").Replace("$", "");
+
+
+
 
             string Lname = txtLName.Text;
+            //Numbers
             Lname = Lname.Replace("1", "").Replace("2", "").Replace("3", "").Replace("4", "").Replace("5", "").Replace("6", "").Replace("7", "").Replace("8", "").Replace("9", "").Replace("0", "");
+            //Symbols
+            Lname = Lname.Replace("%", "").Replace("^", "").Replace("&", "").Replace("*", "").Replace("(", "").Replace(")", "").Replace("_", "").Replace("=", "").Replace("+", "");
+            Lname = Lname.Replace("\\", "").Replace("|", "").Replace("<", "").Replace(">", "").Replace(",", "").Replace(";", "").Replace(":", "").Replace("[", "").Replace("]", "");
+            Lname = Lname.Replace("{", "").Replace("}", "").Replace("'", "").Replace("!", "").Replace("@", "").Replace("#", "").Replace("$", "");
+
+
 
             //Address
             string StreetNum = txtStNo.Text;
+            //Alphabet
+            StreetNum = StreetNum.Replace("a", "").Replace("b", "").Replace("c", "").Replace("d", "").Replace("e", "").Replace("f", "").Replace("g", "").Replace("8", "").Replace("h", "").Replace("i", "");
+            StreetNum = StreetNum.Replace("j", "").Replace("k", "").Replace("l", "").Replace("n", "").Replace("o", "").Replace("p", "").Replace("q", "").Replace("r", "").Replace("s", "").Replace("t", "");
+            StreetNum = StreetNum.Replace("u", "").Replace("v", "").Replace("w", "").Replace("x", "").Replace("y", "");
 
             string StreetName = txtSTName.Text;
 
@@ -40,12 +65,15 @@ namespace ContactTracing
 
             //Phone and Email
             string PhoneNum = txtPhoneN.Text;
+            //Alphabet
             PhoneNum = PhoneNum.Replace("a", "").Replace("b", "").Replace("c", "").Replace("d", "").Replace("e", "").Replace("f", "").Replace("g", "").Replace("8", "").Replace("h", "").Replace("i", "");
             PhoneNum = PhoneNum.Replace("j", "").Replace("k", "").Replace("l", "").Replace("n", "").Replace("o", "").Replace("p", "").Replace("q", "").Replace("r", "").Replace("s", "").Replace("t", "");
-            PhoneNum = PhoneNum.Replace("u", "").Replace("v", "").Replace("w", "").Replace("x", "").Replace("y", "").Replace("z", "").Replace("!", "").Replace("@", "").Replace("#", "").Replace("$", "");
+            PhoneNum = PhoneNum.Replace("u", "").Replace("v", "").Replace("w", "").Replace("x", "").Replace("y", "");
+            
+            //Symbols
             PhoneNum = PhoneNum.Replace("%", "").Replace("^", "").Replace("&", "").Replace("*", "").Replace("(", "").Replace(")", "").Replace("_", "").Replace("-", "").Replace("=", "").Replace("+", "");
             PhoneNum = PhoneNum.Replace("\\", "").Replace("|", "").Replace("<", "").Replace(">", "").Replace(",", "").Replace(".", "").Replace(";", "").Replace(":", "").Replace("[", "").Replace("]", "");
-            PhoneNum = PhoneNum.Replace("{", "").Replace("}", "").Replace("'", "");
+            PhoneNum = PhoneNum.Replace("{", "").Replace("}", "").Replace("'", "").Replace("!", "").Replace("@", "").Replace("#", "").Replace("$", "");
 
             string Email = txtEmail.Text;
 
