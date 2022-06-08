d170a4636d88b7bde731be745cac207b8e5580fb
JohanJVillanueva <johanjvillanueva@iskolarngbayan.pup.edu.ph>
2022-06-08 22:02:00 +0800

    Added and names labels on to the program for Name, Address and Contact No.

--- ContactTracing/Form1.Designer.cs
+++ ContactTracing/Form1.Designer.cs
@@ -29,19 +29,207 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pctBg = new System.Windows.Forms.PictureBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblContact = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblFName = new System.Windows.Forms.Label();
+            this.lblMI = new System.Windows.Forms.Label();
+            this.lblLName = new System.Windows.Forms.Label();
+            this.ltlStN = new System.Windows.Forms.Label();
+            this.lblStreet = new System.Windows.Forms.Label();
+            this.lblProvince = new System.Windows.Forms.Label();
+            this.lblPhoneN = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.txtMI = new System.Windows.Forms.TextBox();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBg)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pctBg
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(566, 788);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pctBg.Image = ((System.Drawing.Image)(resources.GetObject("pctBg.Image")));
+            this.pctBg.Location = new System.Drawing.Point(12, 12);
+            this.pctBg.Name = "pctBg";
+            this.pctBg.Size = new System.Drawing.Size(566, 788);
+            this.pctBg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctBg.TabIndex = 0;
+            this.pctBg.TabStop = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Poetsen One", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblName.ForeColor = System.Drawing.Color.Brown;
+            this.lblName.Location = new System.Drawing.Point(32, 135);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(66, 23);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name:";
+            // 
+            // lblContact
+            // 
+            this.lblContact.AutoSize = true;
+            this.lblContact.Font = new System.Drawing.Font("Poetsen One", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblContact.ForeColor = System.Drawing.Color.Brown;
+            this.lblContact.Location = new System.Drawing.Point(32, 520);
+            this.lblContact.Name = "lblContact";
+            this.lblContact.Size = new System.Drawing.Size(120, 23);
+            this.lblContact.TabIndex = 2;
+            this.lblContact.Text = "Contact No.:";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Poetsen One", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblAddress.ForeColor = System.Drawing.Color.Brown;
+            this.lblAddress.Location = new System.Drawing.Point(32, 284);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(88, 23);
+            this.lblAddress.TabIndex = 3;
+            this.lblAddress.Text = "Address:";
+            // 
+            // lblFName
+            // 
+            this.lblFName.AutoSize = true;
+            this.lblFName.BackColor = System.Drawing.Color.DarkCyan;
+            this.lblFName.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblFName.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblFName.Font = new System.Drawing.Font("Poetsen One", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblFName.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblFName.Location = new System.Drawing.Point(40, 170);
+            this.lblFName.Name = "lblFName";
+            this.lblFName.Size = new System.Drawing.Size(112, 25);
+            this.lblFName.TabIndex = 4;
+            this.lblFName.Text = "First Name:";
+            this.lblFName.Click += new System.EventHandler(this.lblFName_Click);
+            // 
+            // lblMI
+            // 
+            this.lblMI.AutoSize = true;
+            this.lblMI.BackColor = System.Drawing.Color.DarkCyan;
+            this.lblMI.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMI.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblMI.Font = new System.Drawing.Font("Poetsen One", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMI.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblMI.Location = new System.Drawing.Point(257, 170);
+            this.lblMI.Name = "lblMI";
+            this.lblMI.Size = new System.Drawing.Size(40, 25);
+            this.lblMI.TabIndex = 5;
+            this.lblMI.Text = "M.I";
+            // 
+            // lblLName
+            // 
+            this.lblLName.AutoSize = true;
+            this.lblLName.BackColor = System.Drawing.Color.DarkCyan;
+            this.lblLName.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblLName.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblLName.Font = new System.Drawing.Font("Poetsen One", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLName.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblLName.Location = new System.Drawing.Point(325, 170);
+            this.lblLName.Name = "lblLName";
+            this.lblLName.Size = new System.Drawing.Size(109, 25);
+            this.lblLName.TabIndex = 6;
+            this.lblLName.Text = "Last Name:";
+            // 
+            // ltlStN
+            // 
+            this.ltlStN.AutoSize = true;
+            this.ltlStN.BackColor = System.Drawing.Color.DarkCyan;
+            this.ltlStN.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ltlStN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ltlStN.Font = new System.Drawing.Font("Poetsen One", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ltlStN.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.ltlStN.Location = new System.Drawing.Point(40, 324);
+            this.ltlStN.Name = "ltlStN";
+            this.ltlStN.Size = new System.Drawing.Size(68, 25);
+            this.ltlStN.TabIndex = 7;
+            this.ltlStN.Text = "St. No.";
+            // 
+            // lblStreet
+            // 
+            this.lblStreet.AutoSize = true;
+            this.lblStreet.BackColor = System.Drawing.Color.DarkCyan;
+            this.lblStreet.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblStreet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblStreet.Font = new System.Drawing.Font("Poetsen One", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblStreet.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblStreet.Location = new System.Drawing.Point(127, 324);
+            this.lblStreet.Name = "lblStreet";
+            this.lblStreet.Size = new System.Drawing.Size(122, 25);
+            this.lblStreet.TabIndex = 8;
+            this.lblStreet.Text = "Street Name";
+            // 
+            // lblProvince
+            // 
+            this.lblProvince.AutoSize = true;
+            this.lblProvince.BackColor = System.Drawing.Color.DarkCyan;
+            this.lblProvince.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblProvince.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblProvince.Font = new System.Drawing.Font("Poetsen One", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblProvince.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblProvince.Location = new System.Drawing.Point(40, 400);
+            this.lblProvince.Name = "lblProvince";
+            this.lblProvince.Size = new System.Drawing.Size(134, 25);
+            this.lblProvince.TabIndex = 9;
+            this.lblProvince.Text = "City/Province";
+            // 
+            // lblPhoneN
+            // 
+            this.lblPhoneN.AutoSize = true;
+            this.lblPhoneN.BackColor = System.Drawing.Color.DarkCyan;
+            this.lblPhoneN.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPhoneN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblPhoneN.Font = new System.Drawing.Font("Poetsen One", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPhoneN.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblPhoneN.Location = new System.Drawing.Point(40, 555);
+            this.lblPhoneN.Name = "lblPhoneN";
+            this.lblPhoneN.Size = new System.Drawing.Size(143, 25);
+            this.lblPhoneN.TabIndex = 10;
+            this.lblPhoneN.Text = "Phone Number";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.BackColor = System.Drawing.Color.DarkCyan;
+            this.lblEmail.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblEmail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblEmail.Font = new System.Drawing.Font("Poetsen One", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblEmail.Location = new System.Drawing.Point(262, 555);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(70, 25);
+            this.lblEmail.TabIndex = 11;
+            this.lblEmail.Text = "E-mail";
+            // 
+            // txtFName
+            // 
+            this.txtFName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFName.Font = new System.Drawing.Font("Poetsen One", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtFName.Location = new System.Drawing.Point(40, 198);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(211, 40);
+            this.txtFName.TabIndex = 12;
+            this.txtFName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtMI
+            // 
+            this.txtMI.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMI.Font = new System.Drawing.Font("Poetsen One", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtMI.Location = new System.Drawing.Point(257, 198);
+            this.txtMI.Name = "txtMI";
+            this.txtMI.Size = new System.Drawing.Size(62, 40);
+            this.txtMI.TabIndex = 13;
+            // 
+            // txtLName
+            // 
+            this.txtLName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLName.Font = new System.Drawing.Font("Poetsen One", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtLName.Location = new System.Drawing.Point(325, 198);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(228, 40);
+            this.txtLName.TabIndex = 14;
             // 
             // Form1
             // 
@@ -49,20 +237,49 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Linen;
             this.ClientSize = new System.Drawing.Size(592, 780);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.txtMI);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblPhoneN);
+            this.Controls.Add(this.lblProvince);
+            this.Controls.Add(this.lblStreet);
+            this.Controls.Add(this.ltlStN);
+            this.Controls.Add(this.lblLName);
+            this.Controls.Add(this.lblMI);
+            this.Controls.Add(this.lblFName);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblContact);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.pctBg);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LilJohn Contact Tracing Form";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBg)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private PictureBox pictureBox1;
+        private PictureBox pctBg;
+        private Label lblName;
+        private Label lblContact;
+        private Label lblAddress;
+        private Label lblFName;
+        private Label lblMI;
+        private Label lblLName;
+        private Label ltlStN;
+        private Label lblStreet;
+        private Label lblProvince;
+        private Label lblPhoneN;
+        private Label lblEmail;
+        private TextBox txtFName;
+        private TextBox txtMI;
+        private TextBox txtLName;
     }
 }
--- ContactTracing/Form1.cs
+++ ContactTracing/Form1.cs
@@ -6,5 +6,15 @@ namespace ContactTracing
         {
             InitializeComponent();
         }
+
+        private void lblFName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
